2cab4bf51affb8850097302731e91aa23ecd58d4
Your Name <you@example.com>
2024-11-03 22:01:00 +0000

    Installed React template

---
+++ src/components/HomePage/HomePage.jsx
@@ -0,0 +1,38 @@
+import Table from '../Table/Table';
+import Button from '../Button/Button';
+import Navbar from '../Navbar/Navbar';
+import Card from '../Card/Card';
+import './homePage.scss';
+
+function HomePage() {
+
+  return (
+    <div className='container'>
+      <div className="right__side-wrapper">
+        <div className="top__wrapper">
+            <div className="navbar__wrapper">
+              <Navbar />
+            </div>
+          <div className="card__wrapper">
+            <Card direction='horizontal' />
+            <h2>Important headline related to the Card</h2>
+          </div>
+        </div>
+        <div className="main__content-wrapper">
+          <div className="header__wrapper">
+            <h3 className="title__wrapper">Important Data</h3>
+            <div className="buttons__wrapper">
+              <Button variant='primary' />
+              <Button variant='secondary' />
+            </div>
+          </div>
+          <div className="table__wrapper">
+            <Table />
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default HomePage;
